9e226e1334e18152de187d4fcb8a4a68d328595d
Mert Güney <mert_guney_29@hotmail.com>
2022-06-28 22:27:53 +0300

    First&Functional Comps - Bootstrap Configuration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import User from "./components/User";
 
 function App() {
   const test = 35;
   const isAuth = true;
   return (
-    <div>
-      <h1>JSX Operasyon</h1>
+    <div className="container">
+      <h1 className="header">JSX Operasyon</h1>
       <h4>1 + 1 Toplam : {1 + 1}</h4>
       <h4>ToUpperCase : {"mert".toUpperCase()}</h4>
       <h4>Const Değer : {test}</h4>
-      <div>{isAuth ? <p> Kullanıcı kayıtlı</p> : <p>Kullanıcı kayıtlı değil</p>}</div>
+      <div>
+        {isAuth ? <p> Kullanıcı kayıtlı</p> : <p>Kullanıcı kayıtlı değil</p>}
+      </div>
+      <hr />
+      <h1>İlk Component</h1>
+      <User />
+      <hr />
+      <h1>Functional Component</h1>
+      <Navbar />
     </div>
   );
 }
